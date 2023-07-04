07979ddcc05fdf5019c17cf3126a967d512049ce
GuGihyeon <ggh4177@gmail.com>
2023-07-04 18:51:01 +0900

    TestController.java

---
+++ silver/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController	// 이걸 가장 많이 사용한다. Controller + ResponseBody
+public class TestController {
+	
+	@GetMapping("/test")
+//	@ResponseBody // 이걸 붙이지 않아도 된다.
+	public String getTest() {
+		return "테스트!";
+	}
+}
