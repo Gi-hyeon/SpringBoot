d7619796b95a4d69dab0ba8eb4b2c45f2a676216
GuGihyeon <ggh4177@gmail.com>
2023-07-04 18:51:07 +0900

    SujinVO.java

---
+++ silver/src/main/java/com/example/demo/vo/SujinVO.java
@@ -0,0 +1,14 @@
+package com.example.demo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class SujinVO {
+	private int sujinNum;
+	private String sujinName;
+	private String sujinContent;
+}
